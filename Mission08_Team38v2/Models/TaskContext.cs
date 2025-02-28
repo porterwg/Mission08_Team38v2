c08fa3934dedf975af45cfa573ed46bda6214ca9
Jordan Goddard <jordangoddard6@gmail.com>
2025-02-28 13:50:13 -0700

    Remove unnecessary stuff from HomeController

---
+++ Mission08_Team38v2/Models/TaskContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Mission08_Team38v2.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
