c20409ab5eb1a81294d1b6c9e49c50a7347c9ef2
porterwg <porterwg@byu.edu>
2025-02-28 13:29:13 -0700

    Added psuedo code for future home controller functionality i.e. creating context in homecontroller

--- Mission08_Team38v2/Controllers/HomeController.cs
+++ Mission08_Team38v2/Controllers/HomeController.cs
@@ -6,8 +6,21 @@ namespace Mission08_Team38v2.Controllers;
 
 public class HomeController : Controller
 {
+    // private repo _repo;
+    //
+    // public HomeController(repo temp)
+    // {
+    //     _repo = temp;
+    // }
 
+    [HttpGet]
     public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Quadrants()
     {
         return View();
     }
