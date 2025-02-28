324e2fac5bbc6a771d1f2b6afab54206adb37d59
James Erickson <je7erickson@gmail.com>
2025-02-28 13:16:59 -0700

    I made the Quadrant.cshtml page for the maine view

---
+++ Mission08_Team38v2/Views/Home/Quadrants.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mission08_Team38v2.Views.Home
+{
+    public class QuadrantsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
