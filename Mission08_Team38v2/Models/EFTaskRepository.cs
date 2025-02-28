8e2182b5bfe4d4045a9b77db2f3deb44e4b895e4
Jordan Goddard <jordangoddard6@gmail.com>
2025-02-28 15:02:25 -0700

    Add and set up interface and EF repository

---
+++ Mission08_Team38v2/Models/EFTaskRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace Mission08_Team38v2.Models
+{
+    public class EFTaskRepository : ITaskRepository
+    {
+        private TaskContext _context;
+        public EFTaskRepository(TaskContext temp)
+        {
+            _context = temp;
+        }
+
+        public List<Task> Tasks => _context.Tasks.ToList();
+        public List<Category> Categories => _context.Categories.ToList();
+
+        public void AddTask(Task task)
+        {
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+        }
+
+        public void DeleteTask(Task task)
+        {
+            _context.Tasks.Remove(task);
+            _context.SaveChanges();
+        }
+
+        public void EditTask(Task task)
+        {
+            _context.Tasks.Update(task);
+            _context.SaveChanges();
+        }
+    }
+}
