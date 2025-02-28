36bb882553eeefd8cdf452fbecf6212395e3f626
Jordan Goddard <jordangoddard6@gmail.com>
2025-02-28 13:11:10 -0700

    Setting up models

--- Mission08_Team38v2/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Mission08_Team38v2.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
---
+++ Mission08_Team38v2/Models/Task.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission08_Team38v2.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+
+        [Required]
+        public string TaskName { get; set; }
+
+        public DateOnly? DueDate { get; set; }
+
+        [Required]
+        public int Quadrant { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+
+        public bool? Completed { get; set; }
+
+
+    }
+}
